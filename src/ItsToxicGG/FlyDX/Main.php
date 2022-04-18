3539dcf840f1708e2fa7195de21b675f2a7e07e5
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-18 14:03:41 +0200

    Not in-dev

--- src/ItsToxicGG/FlyDX/Main.php
+++ src/ItsToxicGG/FlyDX/Main.php
@@ -18,24 +18,31 @@ class Main extends PluginBase{
   }
   
   public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-      switch ($command->getName()){
-          case "fly"
-            $sender->sendMessage("This command is currently not to use!");
-          break;
+      if($cmd->getName() === "fly"){
+          if($sender intanceof Player){
+              $this->form($sender);
+         } else	{
+              $sender->sendMessage("§cYou Are Not A Player!");
+         }
+      }
+
+      return true; 
+
   }
     
   public function form($player){
       $form = new CustomForm(function(Player $player, array $data)){ 
           if($data === null){
-            case true;
-            
-            break;
-            
-            case false;
-            
-            break;
-            return true;
           }
+          switch($data[1])){
+              case true:
+                  $player->sendMessage("§aFly Is Active");
+              break;
+
+              case false:
+                  $player->sendMessage("§cFly is Not Active"); 
+              break;
+          } 
       });
       $form->setTitle("FlyDX UI");
       $form->addLabel("FlyDX Settings, Off OR On");

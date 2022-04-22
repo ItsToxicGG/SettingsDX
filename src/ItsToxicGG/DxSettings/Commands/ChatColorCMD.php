80cbbdb4b34e35ad51daa8f89a1d8d64e8d353fb
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-22 14:54:34 +0200

    Create ChatColorCMD.php

---
+++ src/ItsToxicGG/DxSettings/Commands/ChatColorCMD.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ItsToxicGG\DxSettings\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class ChatColorCMD extends Command{
+  // I will begin it
+}
